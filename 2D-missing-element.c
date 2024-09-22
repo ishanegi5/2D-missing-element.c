fd34a1c035431b60850a43a487e22a466ad81271
Isha Negi <itsishanegi@gmail.com>
2024-09-22 16:35:06 +0530

    Add files via upload

---
+++ 2D-missing-element.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main()
+
+{ int x[4][2]={{3,},{31,12},{34,54},{553,}};
+  
+for(int i=0;i<4;i++)
+  
+{ for(int j=0;j<2;j++)
+{
+       
+printf("(%d , %d)= %d \n",i,j,x[i][j]);
+  
+}
+
+}
+
+}
